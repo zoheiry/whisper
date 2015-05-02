f0858fcd42158190f59cb8d4c252bac831f2dc91
Kareem <lkareem.alil@gmail.com>
2015-05-02 23:00:13 +0300

    Adds location sharing functionality

---
+++ app/controllers/location_sharing_controller.rb
@@ -0,0 +1,10 @@
+class LocationSharingController < ApplicationController
+	
+	def index
+		#for development purposes
+		if Rails.env.development?
+     		env['REMOTE_ADDR'] = "71.212.123.5"
+    	end
+		@location = request.location.address
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502094054) do
+ActiveRecord::Schema.define(version: 20150502195801) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
