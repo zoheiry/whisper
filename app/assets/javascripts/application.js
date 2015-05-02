66ba8718690b6bebe83b2ee1bb2e6bff7adcbc68
Adam G <ghanem.adam@gmail.com>
2015-05-03 01:57:23 +0300

    priv key bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -94,7 +94,7 @@ $(document).ready(function() {
   if($("#keys-window").attr("data-view-name")) {
     genkey();
     public_key =  "Your public key: " + getPublicKey();
-    private_key =  "Your private key: " + getPublicKey() + "(keep this key safe because you will need it)";
+    private_key =  "Your private key: " + getPrivateKey() + "(keep this key safe because you will need it)";
 
     $("#public_k").html(public_key);
     $("#private_k").html(private_key);
