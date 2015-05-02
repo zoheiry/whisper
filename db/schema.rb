ab676c32fa5e2f42c41189ad842f3d1a650bc54c
zoheiry <ali.elzoheiry@gmail.com>
2015-05-02 20:59:14 +0300

    adding migration gravatar to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502160746) do
+ActiveRecord::Schema.define(version: 20150502175550) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "u1_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150502160746) do
     t.datetime "updated_at"
     t.string   "username"
     t.string   "public_key"
+    t.string   "gravatar_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
