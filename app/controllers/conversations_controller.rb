639005bec168766476ac3256d8e73e47a80be9d1
zoheiry <ali.elzoheiry@gmail.com>
2015-05-02 20:21:05 +0300

    undoing some changes

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,25 @@
 class ConversationsController < ApplicationController
+
+	# def subscribe
+	# 	pubnub = Pubnub.new(
+	# 		:publish_key   => 'pub-c-986edf90-6086-4572-b43b-4ac6883ca4e6',
+	# 		:subscribe_key => 'sub-c-69ec4ef8-bbfd-11e3-b6e0-02ee2ddab7fe'
+	# 	)
+	# 	pubnub.subscribe(
+	# 		:channel  => params[:channel_name]
+	# 	) { |data| puts data.msg }
+	# 	render :json => {status: "Successfully subscribed to channel " + params[:channel_name].to_s}
+	# end
+
+	# def publish
+	# 	pubnub = Pubnub.new(
+	# 		:publish_key   => 'pub-c-986edf90-6086-4572-b43b-4ac6883ca4e6',
+	# 		:subscribe_key => 'sub-c-69ec4ef8-bbfd-11e3-b6e0-02ee2ddab7fe'
+	# 	)
+	# 	pubnub.publish(
+	# 		:channel  => params[:channel_name],
+	# 		:message => params[:message],
+	# 	) { |data| puts data.response }
+	# 	render :json => {status: "published " + params[:message].to_s + " to channel " + params[:channel_name].to_s}
+	# end
 end
