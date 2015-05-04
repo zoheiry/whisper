602a8496be5517767dffab575ddd9408b94e12dd
Kareem <lkareem.alil@gmail.com>
2015-05-04 04:33:00 +0300

    something

--- app/controllers/location_sharing_controller.rb
+++ app/controllers/location_sharing_controller.rb
@@ -2,6 +2,7 @@ class LocationSharingController < ApplicationController
 	
 	def index
 		#for development purposes
+		#debugger
 		if Rails.env.development?
      		env['REMOTE_ADDR'] = "71.212.123.5"
     	end
